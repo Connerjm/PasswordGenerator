344f6a0d7a72b9bfc967257efce7d198b97998f9
Conner Martin <connerjm2@gmail.com>
2021-01-04 19:12:21 -0800

    Added some comments and added a modal for getting the password options from the user.

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword()
+// // Write password to the #password input
+// function writePassword()
+// {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+// Declaring the variables.
+var modal = document.getElementById("modal");
+var generateBtn = document.getElementById("generate");
+
+// Close the popup.
+function close()
 {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  modal.style.display = "none";
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Open the popup when the button is clicked.
+generateBtn.onclick = function ()
+{
+  modal.style.display = "block";
+}
+
+// Closes the popup if anything other that the popup is clicked.
+window.onclick = function (event) {
+  if (event.target == modal) {
+    close();
+  }
+}
